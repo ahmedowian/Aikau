e1b24a49e10835c4c1d441bb0922589c78a5392b
Martin Doyle <martin.doyle@alfresco.com>
2015-09-03 10:42:05 +0100

    Add support for BrowserStack testing

--- aikau/src/grunt/testing.js
+++ aikau/src/grunt/testing.js
@@ -13,6 +13,7 @@ module.exports = function(grunt) {
    // Register test tasks for local/vagrant/SauceLabs/grid respectively
    grunt.registerTask("test_local", ["startUnitTestApp", "waitServer", "clean:testScreenshots", "generate-require-everything", "intern:local"]);
    grunt.registerTask("test", ["startUnitTestApp", "waitServer", "clean:testScreenshots", "generate-require-everything", "intern:dev"]);
+   grunt.registerTask("test_bs", ["startUnitTestApp", "waitServer", "clean:testScreenshots", "generate-require-everything", "intern:bs"]);
    grunt.registerTask("test_sl", ["startUnitTestApp", "waitServer", "clean:testScreenshots", "generate-require-everything", "intern:sl"]);
    grunt.registerTask("test_grid", ["waitServer", "clean:testScreenshots", "generate-require-everything", "intern:grid"]);
 
@@ -77,6 +78,13 @@ module.exports = function(grunt) {
       intern: {
          options: {
             rowsCols: process.stdout.rows + "|" + process.stdout.columns // Used by ConcurrentReporter
+         },
+         bs: {
+            options: {
+               runType: "runner",
+               config: "src/test/resources/intern_bs",
+               useLocalhost: true
+            }
          },
          dev: {
             options: {
